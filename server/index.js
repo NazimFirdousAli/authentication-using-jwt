ce5d3bbbdb7752155c6a41a45bb2634447361ba2
NazimFirdousAli <51528955+NazimFirdousAli@users.noreply.github.com>
2021-02-13 21:59:40 +0500

    Update the prisma schema with image upload feature and set the image upload on server Not set in Client

--- server/index.js
+++ server/index.js
@@ -2,24 +2,59 @@ const { ApolloServer, gql } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient();
 
+const path = require('path')
+const fs = require('fs')
+
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { APP_SECRET } = require('./utils.js')
+const { APP_SECRET } = require('./utils.js');
 
 const resolvers = {
   Query: {
     users: async () => { return await prisma.user.findMany() },
   },
   Mutation: {
-    signUp: async (root, { password, ...data }, context, info) => {
-
-      const hashpassword = bcrypt.hashSync(password, 10)
-
-      data.password = hashpassword;
-      const uniqueEmail = await prisma.user.findUnique({where:{email:data.email}})
-      if(uniqueEmail){
+    signUp: async (root, { password, image, ...data }, context, info) => {
+      const uniqueEmail = await prisma.user.findUnique({ where: { email: data.email } })
+      if (uniqueEmail) {
         throw new Error("Email exists")
       }
+
+      const hashpassword = bcrypt.hashSync(password, 10)
+      data.password = hashpassword;
+
+      //   data.image = await new Promise((resolve, reject) => {
+      //     return image.then(({ createReadStream, ...rest })=> {
+      //         const id = Math.random().toString(32).substr(7);
+      //         const filename = `${id}-${rest.filename}`;
+      //         if (!fs.existsSync("./uploads")) fs.mkdirSync("./uploads");
+      //         createReadStream()
+      //             .pipe(fs.createWriteStream(path.join('./uploads', filename)))
+      //             .on('error', (error) => reject(new Error(error.message)))
+      //             .on('finish', () => resolve(filename));
+      //     })
+      // })
+
+
+      // data.image = await new Promise((resolve, reject) => {
+      // return image.then(image => {
+      //   console.log(image)
+      //   const { createReadStream, filename } = image
+      //   const stream = createReadStream()
+      //   const pathName = path.join(__dirname, `./uploads/${filename}`)
+      //  resolve(stream.pipe(fs.createWriteStream(pathName))) 
+      //  reject(new Error("Not found"))
+      // })
+      // })
+      // ?
+
+      const { createReadStream, filename } = await image
+      const stream = createReadStream()
+      const pathName = path.join(__dirname, `./uploads/${filename}`)
+      await stream.pipe(fs.createWriteStream(pathName))
+      const id = Math.random().toString(32).substr(7);
+      data.image = `${id}-${filename}`
+
 
       const createdUser = await prisma.user.create({ data });
       const token = jwt.sign({ userId: createdUser.id }, APP_SECRET);
@@ -56,6 +91,7 @@ const typeDefs = gql`
   email: String! 
   name: String!
   password: String!
+  image:String
 }
 type AuthPayload{
   token: String!
@@ -65,7 +101,7 @@ type AuthPayload{
     users: [Users]
   }
   type Mutation{
-      signUp(email: String!, name: String!, password: String!):AuthPayload!
+      signUp(email: String!, name: String!, password: String!,image: Upload):AuthPayload!
       login(email: String!, password: String!):AuthPayload!
 
   }
